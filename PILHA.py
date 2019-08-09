5cf9d8df17c62b088e905bcd0655f0ace3124709
Luis Henrique Bizinelli <luis_bizinelli@hotmail.com>
2019-08-09 20:27:58 -0300

    Update PILHA.py
    update

--- PILHA.py
+++ PILHA.py
@@ -1,14 +1,29 @@
-#pilha
+Python 2.7.15 (v2.7.15:ca079a3ea3, Apr 30 2018, 16:30:26) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:/Users/opet/Desktop/PILHA.py ==================
+[20, 10, 40, 30]
+[20, 10, 40, 30, 50]
+[20, 10, 40, 30]
 
-pilha = [20,10,40,30]
-
-print(pilha)
-
-pilha.append(50)
-
-print(pilha)
-
-valor = pilha.pop()
-
-print(pilha)
-print(valor)
+Traceback (most recent call last):
+  File "C:/Users/opet/Desktop/PILHA.py", line 14, in <module>
+    print(valor)
+NameError: name 'valor' is not defined
+>>> 
+================== RESTART: C:/Users/opet/Desktop/PILHA.py ==================
+[20, 10, 40, 30]
+[20, 10, 40, 30, 50]
+[20, 10, 40, 30]
+50
+>>> 
+================== RESTART: C:/Users/opet/Desktop/PILHA.py ==================
+[20, 10, 40, 30]
+[20, 10, 40, 30, 50]
+[20, 10, 40, 30]
+50
+[20, 10, 40, 30]
+[20, 10, 40, 30, 50]
+[10, 40, 30, 50]
+20
+>>> 
